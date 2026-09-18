d899fefe5e3d758386d9bb56ec67c660568ddf94
C9hihihi <nvdkhanh2006thptahhnd@gmail.com>
2026-09-18 11:37:00 +0700

    Add files via upload

--- PRJ301Ftes/src/java/controller/LoginController.java
+++ PRJ301Ftes/src/java/controller/LoginController.java
@@ -13,40 +13,27 @@ public class LoginController extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginController</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        if (username.equals("Admin") && password.equals("123")) {
+        if ("Admin".equals(username) && "123".equals(password)) {
             out.println("Login Successfully!!!");
         } else {
             out.println("Login Failed!!");
         }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     @Override
